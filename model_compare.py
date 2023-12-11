269adf12640696510c654a5effcdf6b7bcaef69a
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2023-12-11 23:31:53 +0100

    working script that compares LLM models with one imput.

--- model_compare.py
+++ model_compare.py
@@ -1,8 +1,16 @@
+# this file is used to test the performance of the models and store the results in a database
+# it tests a model and then frees up the memory before testing the next model
+# this is to avoid memory issues when testing multiple models
+# it also logs the results to a log file
+# the log file can be used to check if any errors occurred during testing
+# the log file can also be used to check the performance of each model
 import psutil
 import time
 import duckdb
 import logging
 import gc
+import sys
+import os
 from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
@@ -10,21 +18,21 @@ from transformers import (
     T5ForConditionalGeneration,
 )
 import transformers
-import os
-import sys
 
-# this file is used to test the performance of the models and store the results in a database
-# it tests a model and then frees up the memory before testing the next model
-# this is to avoid memory issues when testing multiple models
-# it also logs the results to a log file
-# the log file can be used to check if any errors occurred during testing
-# the log file can also be used to check the performance of each model
+MAX_NEW_TOKENS = 200  # Maximum number of new tokens to generate
+
+# Set NUMEXPR_MAX_THREADS to the number of available cores minus one
+num_cores = psutil.cpu_count(logical=False)  # Get physical core count
+if num_cores and num_cores > 1:
+    os.environ['NUMEXPR_MAX_THREADS'] = str(num_cores - 1)
+else:
+    os.environ['NUMEXPR_MAX_THREADS'] = '1'  # Fallback to 1 if single-core or unable to determine
 
 
 # Initialize logging
-#logging.basicConfig(
-#    filename="model_testing.log", level=logging.DEBUG, format="%(asctime)s %(message)s"
-#)
+logging.basicConfig(
+    filename="model_testing.log", level=logging.INFO, format="%(asctime)s: %(message)s"
+)
 
 # Connect to DuckDB
 conn = duckdb.connect("model_results.duckdb")
@@ -37,14 +45,13 @@ def get_model_size(model_name):
     cache_dir = transformers.file_utils.default_cache_path
     model_dir = os.path.join(cache_dir, model_name)
     total_size = 0
-
     for dirpath, dirnames, filenames in os.walk(model_dir):
         for f in filenames:
             fp = os.path.join(dirpath, f)
             total_size += os.path.getsize(fp)
+    return total_size / (1024 * 1024)  # Convert bytes to megabytes
 
 
-# Function to measure performance and store in DB
 def measure_performance_and_store(model_func, model_name, input_text, tokenizer, conn):
     try:
         logging.info(f"Testing model: {model_name}")
@@ -77,7 +84,7 @@ def measure_performance_and_store(model_func, model_name, input_text, tokenizer,
         )
 
         logging.info(
-            f"Model {model_name} tested successfully. Execution Time: {execution_time}, CPU Usage: {cpu_usage}%, Memory Usage: {memory_usage} MB, Model Size: {model_size} MB"
+            f"Model {model_name} tested successfully. Execution Time: {execution_time}, CPU Usage: {cpu_usage}%, Memory Usage: {memory_usage} MB, Model Size: {model_size} MB \nwith result: \n{result}"
         )
     except Exception as e:
         logging.error(f"Error testing model {model_name}: {str(e)}")
@@ -86,60 +93,46 @@ def measure_performance_and_store(model_func, model_name, input_text, tokenizer,
 # List of models to test
 models_to_test = [
     {"name": "google/flan-t5-small", "type": "seq2seq"},
-    {"name": "google/flan-t5-xxl", "type": "conditional_generation"},
+    #{"name": "google/flan-t5-xxl", "type": "conditional_generation"},
     {"name": "google/flan-t5-large", "type": "conditional_generation"},
     {"name": "MBZUAI/LaMini-Flan-T5-783M", "type": "text2text-generation"},
     # Add other models here
 ]
 
-# Connect to DuckDB
-conn = duckdb.connect("model_results.duckdb")
-conn.execute(
-    "CREATE TABLE IF NOT EXISTS model_performance (input TEXT, model TEXT, execution_time FLOAT, cpu_usage FLOAT, memory_usage FLOAT, model_size FLOAT, output TEXT)"
-)
-
 if __name__ == "__main__":
-    input_text = sys.argv[0]
-    print(input_text)
+    input_text = sys.argv[1]
+    # input_text = sys.argv[1] if len(sys.argv) > 1 else "Default input text"
 
     for model_info in models_to_test:
         model_name = model_info["name"]
         model_type = model_info["type"]
 
-    # Load model and tokenizer
-    # model_func is a function that takes some text, tokenizes it, passes it to the model to generate a response, and then decodes this response back into readable text.
-    if model_type == "seq2seq":
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-        model_func = lambda text, tokenizer: tokenizer.batch_decode(
-            model.generate(**tokenizer(text, return_tensors="pt")),
-            skip_special_tokens=True,
-        )
-    elif model_type == "conditional_generation":
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = T5ForConditionalGeneration.from_pretrained(model_name)
-        model_func = lambda text, tokenizer: tokenizer.batch_decode(
-            model.generate(**tokenizer(text, return_tensors="pt")),
-            skip_special_tokens=True,
-        )
-    elif model_type == "text2text-generation":
-        tokenizer = None
-        model = pipeline("text2text-generation", model=model_name)
-        model_func = lambda text: model(text)[0]["generated_text"]
-    # Add other model types if needed
+        if model_type == "seq2seq":
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+            model_func = lambda text, tok: tok.batch_decode(
+                model.generate(**tok(text, return_tensors="pt"), max_new_tokens=MAX_NEW_TOKENS),
+                skip_special_tokens=True,
+            )
+        elif model_type == "conditional_generation":
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = T5ForConditionalGeneration.from_pretrained(model_name)
+            model_func = lambda text, tok: tok.batch_decode(
+                model.generate(**tok(text, return_tensors="pt"), max_new_tokens=MAX_NEW_TOKENS),
+                skip_special_tokens=True,
+            )
+        elif model_type == "text2text-generation":
+            tokenizer = None
+            model = pipeline("text2text-generation", model=model_name)
+            model_func = lambda text, tok: model(text)[0]["generated_text"]
 
-    # Measure performance
-    measure_performance_and_store(
-        model_func=model_func,
-        model_name=model_name,
-        input_text=input_text,
-        tokenizer=tokenizer,
-        conn=conn,
-    )
+        measure_performance_and_store(
+            model_func, model_name, input_text, tokenizer, conn
+        )
 
-    # Free up memory
-    del model, tokenizer
-    gc.collect()
+        # Free up memory
+        del model, tokenizer
+        gc.collect()
 
     # Close database connection
     conn.close()
